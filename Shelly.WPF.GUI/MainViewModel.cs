b288e4af429777e46e97a9f6f10602018fb1ef14
shogun <iivchenko@live.com>
2015-12-13 11:34:34 +0000

    Add ViewModel

---
+++ Shelly.WPF.GUI/MainViewModel.cs
@@ -0,0 +1,75 @@
+﻿using System.ComponentModel;
+using System.Management.Automation;
+using System.Windows.Input;
+
+namespace Shelly.WPF.GUI
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        private readonly PowerShell _shell;
+
+        private string _script;
+        private string _output;        
+
+        public MainViewModel()
+        {
+            _shell = PowerShell.Create();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string Script
+        {
+            get
+            {
+                return _script;
+            }
+
+            set
+            {
+                _script = value;
+
+                OnPropertyChanged("Script");
+            }
+        }
+
+        public string Output
+        {
+            get
+            {
+                return _output;
+            }
+
+            set
+            {
+                _output= value;
+
+                OnPropertyChanged("Output");
+            }
+        }
+
+        public ICommand Run
+        {
+            get
+            {
+                return new LazyCommand(() =>
+                {
+                    _shell.AddScript(Script);
+
+                    Output += _shell.Invoke();
+
+                });
+            }
+        } 
+
+        private void OnPropertyChanged(string name)
+        {
+            var temp = PropertyChanged;
+
+            if (temp != null)
+            {
+                temp(this, new PropertyChangedEventArgs(name));
+            }
+        }
+    }
+}
